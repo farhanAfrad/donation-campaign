a046192739a1b81e573f1665f7fea06ac3546615
farhanAfrad <farhanafrad796@gmail.com>
2023-11-29 12:52:22 +0600

    all ok

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -5,6 +5,16 @@ const Statistics = () => {
     return (
         <div style={{ height: '500px' }}>
             <CustomPieChart></CustomPieChart>
+            <div className='flex justify-center gap-10'>
+                <div className='flex items-center gap-3'>
+                    <p>Your Donation</p>
+                    <span className='w-24 h-3 bg-[#00C49F] rounded-sm'></span>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <p>Total Donation</p>
+                    <span className='w-24 h-3 bg-[#FF444A] rounded-sm'></span>
+                </div>
+            </div>
         </div>
     );
 };
