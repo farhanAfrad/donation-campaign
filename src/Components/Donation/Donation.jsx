2c631b52c7c2776d12b440d0eaa3ee58d866efc4
farhanAfrad <farhanafrad796@gmail.com>
2023-11-29 12:33:14 +0600

    finally donation page is fixed

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -9,9 +9,17 @@ const Donation = () => {
     const donations = donationItem.filter( item => donatedData.find(data => item.id == data) );
 
     console.log(donations);
+    let flag = '';
 
-    const [donatedItemLength, setDonatedItemLenght] = useState(4)
+    const [donatedItemLength, setDonatedItemLenght] = useState(4);
+    if(donations.length <= 4 || donatedItemLength === donations.length){
+        flag = 'hidden';
+    }
+    else{
+        flag = '';
+    }
 
+    // donatedItemLength === donations.length ? 'hidden': donatedItemLength === 4 ? 'hidden' : ''
 
     return (
         <div>
@@ -20,7 +28,7 @@ const Donation = () => {
                     donations.slice(0,donatedItemLength).map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
                 }
             </div>
-            <div className={`mt-10 text-center ${donatedItemLength === donations.length ? 'hidden': donatedItemLength === 4 ? 'hidden' : ''}`}>
+            <div className={`mt-10 text-center ${flag}`}>
                 <button onClick={()=>setDonatedItemLenght(donations.length)} className='py-3 px-6 rounded-lg bg-green-600 text-white font-semibold'>See All</button>
             </div>
         </div>
